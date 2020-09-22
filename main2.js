0a4c3c0ff347ae7ddb59b675cb13a4b8fceb120a
Jordi Garcia <jordi@tokyoplay.com>
2020-09-22 11:06:49 -0500

    modificando funcion countneigbours

--- main2.js
+++ main2.js
@@ -19,9 +19,9 @@ class celula {
         this.neighbors = Neighbors;
     }
 
-    getStatusSymbol() {
-        cellSymbol(this.status);
-    }
+    // getStatusSymbol() {
+    //     cellSymbol(this.status);
+    // }
 
     getNeighbors() {
         return this.neighbors;
@@ -63,7 +63,7 @@ function initGame() {
                 cell = new celula(1, 0);
                 arrayCells[x][y] = cell;
             }
-            resultBoard+= arrayCells[x][y].getStatusSymbol();
+            resultBoard+= cellSymbol(arrayCells[x][y].getStatus());
         }
         resultBoard += '\n';
     }
@@ -77,7 +77,7 @@ function iteration(ParamarrayCells){
     console.log('Generacion '+(loopControler+1) +' :');
     for (var x = 0; x < ParamarrayCells.length; x++) {
         for (var y = 0; y < ParamarrayCells[x].length; y++) {
-            let neighborsCount = countNeighbors(x,y);
+            let neighborsCount = countNeighbors(ParamarrayCells,x,y);
             ParamarrayCells[x][y].setNeighbors(neighborsCount);
             doaFunction(ParamarrayCells[x][y],x,y);
         }
@@ -90,14 +90,14 @@ function iteration(ParamarrayCells){
     }
 }
 
-function countNeighbors(x,y){
+function countNeighbors(arrayCheck,x,y){
     let neighbors = 0;
     for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
             try {
                 
-                if (ParamarrayCells[x + i][y + j].getStatus() == 1) {
-                    if(ParamarrayCells[x][y].getStatus() == 1 && i==0 && j==0){
+                if (arrayCheck[x + i][y + j].getStatus() == 1) {
+                    if(arrayCheck[x][y].getStatus() == 1 && i==0 && j==0){
                         //console.log('se conto solo');
                     }else{
                         neighbors++;
@@ -114,7 +114,7 @@ function countNeighbors(x,y){
 
 
 function doaFunction(cell,x,y) {
-    console.log(cell);
+    //console.log(cell);
     if ((cell.getStatus() == 1) && (cell.getNeighbors() < 2)) {
         newArray[x][y].setStatus(0);         // Soledad
         return 0;
@@ -133,7 +133,7 @@ function showResult(array){
     resultBoard = '';
     for (let x = 0; x < array.length; x++) {
         for (let y = 0; y < array[x].length; y++) {
-            resultBoard += arrayCells[x][y].getStatusSymbol();
+            resultBoard += cellSymbol(arrayCells[x][y].getStatus());
         }
         resultBoard += '\n';
     }
@@ -153,8 +153,8 @@ function cellSymbol(status) {
 
 
 
-module.exports = initGame;
-//module.exports = doaFunction;
-module.exports = cellSymbol;
-// Aqui tiene que ir el nombre de la funcion
+// module.exports = initGame;
+// //module.exports = doaFunction;
+// module.exports = cellSymbol;
+// // Aqui tiene que ir el nombre de la funcion
 
